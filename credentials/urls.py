6ad2b1084fb0930e520d80546fdcbea69e4d52bd
Aman <106174065+AmanezCode@users.noreply.github.com>
2023-11-07 14:39:27 +0300

    Update urls.py

--- credentials/urls.py
+++ credentials/urls.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from . import v_controller
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 urlpatterns = [
     path('', views.index_view),
