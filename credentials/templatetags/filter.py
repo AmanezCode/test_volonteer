a2076acd7a522e9f13c31efb31cbd6144fa26b98
Aman <106174065+AmanezCode@users.noreply.github.com>
2023-11-07 21:19:41 +0300

    Delete credentials directory

--- credentials/templatetags/filter.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def is_registered(event, username):
-    return event.volunteer_set.filter(profile__userName__username=username).exists()
